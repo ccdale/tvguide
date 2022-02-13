602ec832f49ba4c71e8b13873226a78557ac1460
ccdale <chris.allison@hivehome.com>
2022-02-13 09:43:50 +0000

    Bumping version from 0.3.39 to 0.3.40

--- tests/test_tvguide.py
+++ tests/test_tvguide.py
@@ -2,4 +2,4 @@ from tvguide import __version__
 
 
 def test_version():
-    assert __version__ == "0.3.39"
+    assert __version__ == "0.3.40"
--- tvguide/__init__.py
+++ tvguide/__init__.py
@@ -39,7 +39,7 @@ log.addHandler(jlog)
 log.propagate = False
 log.setLevel(logging.INFO)
 
-__version__ = "0.3.39"
+__version__ = "0.3.40"
 
 db = SQLAlchemy()
 
